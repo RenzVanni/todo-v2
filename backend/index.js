0dd1f6db21d63b7b321f9014782c874b8faee07e
renz vanni bato <renzvanni.dev@gmail.com>
2023-11-26 11:41:07 +0800

    .env backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 const connect = require("./config/db");
 const cors = require("cors");
