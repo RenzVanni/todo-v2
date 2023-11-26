d38aa2d10ab577776230ece5cd27074971af258c
renz vanni bato <renzvanni.dev@gmail.com>
2023-11-26 11:43:14 +0800

    asda

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 const connect = require("./config/db");
 const cors = require("cors");
 
 app.use(cors());
 connect();
 
+const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/", require("./routes/useRoutes"));
